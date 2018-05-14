435e55928003ef691dd3ed096cb012255dd95b36
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-05-14 14:33:39 -0600

    :(

--- udemy-react-16/burgerbuilder/src/components/Burger/Burger.js
+++ udemy-react-16/burgerbuilder/src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import classes from './Burger.css'
 import Ingredients from './Ingredients/Ingredients'
 
 export default props => {
+  const arrayIngredients = Object.keys( props.ingredients ).map( igkey => {
+    return [ ...Array( props.ingredients[ igkey ] ) ].map( ( _, index ) => {
+      <Ingredients key={igkey+index} type={igkey}/>
+    } )
+  } )
+
   return (
     <div className={classes.Burger}>
       <Ingredients type="bread-top"/>
-      <Ingredients type="cheese"/>
-      <Ingredients type="meat"/>
+      {arrayIngredients}
       <Ingredients type="bread-bottom"/>
     </div>
   )
